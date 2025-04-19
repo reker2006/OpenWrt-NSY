d4463e4154a598dace9a1a6a5a95da222f18ca3e
JDKCHINA <935341417@qq.com>
2025-04-19 14:01:36 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -11,7 +11,7 @@
 
 #添加qmodem模组管理
 git clone --depth=1 https://github.com/FUjr/modem_feeds package/modem_feeds
- 
+git clone https://github.com/Siriling/5G-Modem-Support package/luci-app-modem
 
 # rm -rf feeds/luci/themes/luci-theme-argon
 # rm -rf feeds/luci/themes/luci-theme-design
